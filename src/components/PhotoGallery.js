cf60b02c1fda51e7957ae400a93aaa2effdc7e0b
Ty Hyten <ty.hyten@gmail.com>
2020-03-03 12:02:08 -0700

    fetch data and populate gallery

---
+++ src/components/PhotoGallery.js
@@ -0,0 +1,39 @@
+import Img from "gatsby-image"
+import { chunk, sum } from "lodash" // TODO - remove lodash
+import React from "react"
+import { Box } from "rebass" // TODO - add this to package.json
+
+const PhotoGallery = ({ images, itemsPerRow: itemsPerRowByBreakpoints }) => {
+  const aspectRatios = images.map(image => image.aspectRatio) // TODO - replace this with a reduce
+
+  const rowAspectRatioSumsByBreakpoints = itemsPerRowByBreakpoints.map(
+    itemsPerRow =>
+      chunk(aspectRatios, itemsPerRow).map(rowAspectRatios =>
+        sum(rowAspectRatios)
+      )
+  )
+  // TODO - perhaps do this with CSS grid auto-fill/auto-fit to simplify
+
+  return (
+    <div>
+      {images.map((image, i) => (
+        <Box
+          key={image.src}
+          width={rowAspectRatioSumsByBreakpoints.map(
+            (rowAspectRatioSums, j) => {
+              const rowIndex = Math.floor(i / itemsPerRowByBreakpoints[j])
+              const rowAspectRatioSum = rowAspectRatioSums[rowIndex]
+
+              return `${(image.aspectRatio / rowAspectRatioSum) * 100}%`
+            }
+          )}
+          css={{ display: "inline-block" }}
+        >
+          <Img fluid={image} loading="lazy" />
+        </Box>
+      ))}
+    </div>
+  )
+}
+
+export default PhotoGallery
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,10 +1,41 @@
 import React from "react"
 import Header from "../components/Header"
+import PhotoGallery from "../components/PhotoGallery"
+import { useStaticQuery, graphql } from "gatsby"
 
-const Gallery = () => (
-  <Header>
-    <p>this is the gallery</p>
-  </Header>
-)
+const Gallery = ({ data }) => {
+  useStaticQuery(graphql`
+    query {
+      allFile(
+        filter: {
+          extension: { regex: "/(jpg)/" }
+          relativeDirectory: { eq: "images" }
+        }
+      ) {
+        edges {
+          node {
+            childImageSharp {
+              fluid {
+                aspectRatio
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Header>
+      <PhotoGallery
+        images={data.allFile.edges.map(({ node }) => ({
+          ...node.childImageSharp.fluid,
+        }))}
+        itemsPerRow={[1, 3]}
+      />
+    </Header>
+  )
+}
 
 export default Gallery
