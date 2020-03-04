79b9d570b10418065335fc257fccf3767ecb7f88
Ty Hyten <ty.hyten@gmail.com>
2020-03-04 14:13:31 -0700

    add hovering images on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,49 @@
 import React from "react"
 import Header from "../components/Header"
+import Img from "gatsby-image"
+import { graphql, useStaticQuery } from "gatsby"
+import { Box, Flex } from "rebass"
+import "../styles/index.scss"
 
-export default () => (
-  <Header>
-    <div>put photo boxes for code or photos here</div>
-  </Header>
-)
+export default ({ data }) => {
+  useStaticQuery(graphql`
+    query {
+      photographyImage: file(relativePath: { eq: "assets/photography.jpg" }) {
+        childImageSharp {
+          fluid(quality: 80, maxWidth: 400) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      developmentImage: file(relativePath: { eq: "assets/development.jpg" }) {
+        childImageSharp {
+          fluid(quality: 80, maxWidth: 400) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Header>
+      <Flex mt={4} style={{ alignItems: "center" }}>
+        <Box width={1 / 2}>
+          <Img
+            className="nav-image"
+            fluid={data.photographyImage.childImageSharp.fluid}
+          />
+        </Box>
+        <div style={{ margin: "0 40px" }}>
+          <h2>or</h2>
+        </div>
+        <Box width={1 / 2}>
+          <Img
+            className="nav-image"
+            fluid={data.developmentImage.childImageSharp.fluid}
+          />
+        </Box>
+      </Flex>
+    </Header>
+  )
+}
