dc66e29f93c24bb3acd67235a3e6f20295002de6
Ty Hyten <ty.hyten@gmail.com>
2020-03-04 15:18:41 -0700

    make images darker and handle text overlays

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,18 @@ import "../styles/index.scss"
 export default ({ data }) => {
   useStaticQuery(graphql`
     query {
-      photographyImage: file(relativePath: { eq: "assets/photography.jpg" }) {
+      photographyImage: file(
+        relativePath: { eq: "assets/photography-image.jpg" }
+      ) {
         childImageSharp {
           fluid(quality: 80, maxWidth: 400) {
             ...GatsbyImageSharpFluid
           }
         }
       }
-      developmentImage: file(relativePath: { eq: "assets/development.jpg" }) {
+      developmentImage: file(
+        relativePath: { eq: "assets/development-image.jpg" }
+      ) {
         childImageSharp {
           fluid(quality: 80, maxWidth: 400) {
             ...GatsbyImageSharpFluid
@@ -29,19 +33,49 @@ export default ({ data }) => {
     <Header>
       <Flex mt={4} style={{ alignItems: "center" }}>
         <Box width={1 / 2}>
-          <Img
-            className="nav-image"
-            fluid={data.photographyImage.childImageSharp.fluid}
-          />
+          <div style={{ textAlign: "center", position: "relative" }}>
+            <h1
+              style={{
+                color: "white",
+                zIndex: "3",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                fontSize: "3vw",
+              }}
+            >
+              photography
+            </h1>
+            <Img
+              className="nav-image"
+              fluid={data.photographyImage.childImageSharp.fluid}
+            />
+          </div>
         </Box>
         <div style={{ margin: "auto" }}>
           <h3>or</h3>
         </div>
         <Box width={1 / 2}>
-          <Img
-            className="nav-image"
-            fluid={data.developmentImage.childImageSharp.fluid}
-          />
+          <div style={{ textAlign: "center", position: "relative" }}>
+            <h1
+              style={{
+                color: "white",
+                zIndex: "1",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                fontSize: "2em",
+              }}
+            >
+              development
+            </h1>
+            <Img
+              className="nav-image"
+              fluid={data.developmentImage.childImageSharp.fluid}
+            />
+          </div>
         </Box>
       </Flex>
     </Header>
